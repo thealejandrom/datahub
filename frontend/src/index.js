5e21b524c16ef3c71c6fc92e48a05e5eef9a46b0
officialdapulse <admin@da-pulse.com>
2022-02-01 01:16:51 -0800

    note place
    I AM STILL TRYING TO MAKE THE LINKS IN THE HEADER AREA CLICKABLE AND BE ABLE TO
     * REDIRECT TO THE APPROPRIATE PAGE COMPONENT RENDERING. THIS IS WHERE I LEFT OFF.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/*** I AM STILL TRYING TO MAKE THE LINKS IN THE HEADER AREA CLICKABLE AND BE ABLE TO
+ * REDIRECT TO THE APPROPRIATE PAGE COMPONENT RENDERING. THIS IS WHERE I LEFT OFF. ***/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
