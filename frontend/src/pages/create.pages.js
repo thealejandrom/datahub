ac5f58a610fb394819a57ff26ef20f82aba8729d
officialdapulse <admin@da-pulse.com>
2022-02-01 23:54:09 -0800

    fck btch

--- frontend/src/pages/create.pages.js
+++
@@ -1,9 +0,0 @@
-function CreatePage() {
-    return (
-        <div>
-            <h1>Create Page</h1>
-        </div>
-    )
-}
-
-export default CreatePage
--- frontend/src/pages/explore.pages.js
+++
@@ -1,9 +0,0 @@
-function ExplorePage() {
-    return (
-        <div>
-            <h1>Explore Page</h1>
-        </div>
-    )
-}
-
-export default ExplorePage
